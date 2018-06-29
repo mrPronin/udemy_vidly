bf7a62ab25cbbd3561da01c4bc050f1997350002
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-06-29 12:30:29 +0200

    Section 8. 110. Restructuring the Project.

--- routes/customers.js
+++ routes/customers.js
@@ -1,26 +1,6 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const {Customer, validate} = require('../models/customer');
 const express = require('express');
 const router = express.Router();
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 50
-	},
-	isGold: {
-		type: Boolean,
-		default: false
-	},
-	phone: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 50
-	}
-}));
   
 router.get('/', async (req, res) => {
 	const customers = await Customer.find().sort('name');
@@ -28,7 +8,7 @@ router.get('/', async (req, res) => {
 });
   
 router.post('/', async (req, res) => {
-	const { error } = validateCustomer(req.body);
+	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	let customer = new Customer({
@@ -41,7 +21,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-	const { error } = validateCustomer(req.body);
+	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const customer = await Customer.findByIdAndUpdate(req.params.id, { 
@@ -70,14 +50,5 @@ router.get('/:id', async (req, res) => {
 
 	res.send(customer);
 });
-
-function validateCustomer(customer) {
-	const schema = {
-		name: Joi.string().min(5).max(50).required(),
-		phone: Joi.string().min(5).max(50).required(),
-		isGold: Joi.boolean()
-	};
-	return Joi.validate(customer, schema);
-}
 
 module.exports = router;
