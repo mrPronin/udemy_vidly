21413b7ef0f1370473a4db785bbef6e82dcfc611
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-04 13:02:58 +0200

    Section 10. 131. JSON Web Tokens.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
-const _ = require('lodash');
+// const _ = require('lodash');
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
@@ -17,7 +18,9 @@ router.post('/', async (req, res) => {
 	const validPassword = await bcrypt.compare(req.body.password, user.password);
 	if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-	res.send(true);
+	const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+
+	res.send(token);
 });
 
 function validate(req) {
