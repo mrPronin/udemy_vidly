97933096166f45aff77ba75aaadfd36cc51ee321
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-04 23:05:41 +0200

    Section 10. 140. Role Based Authorization.

--- models/user.js
+++ models/user.js
@@ -24,12 +24,13 @@ const userSchema = new mongoose.Schema({
 		required: true,
 		minlength: minlength,
 		maxlength: 1024
-	}
+	},
+	isAdmin: Boolean
 });
 
 // Information Expert Principle
 userSchema.methods.generateAuthToken = function() {
-	const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+	const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
 	return token;
 };
 
