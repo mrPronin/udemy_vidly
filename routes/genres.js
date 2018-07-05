21bdcab1cb4cd2cb3a2bdadcf7a27a30fe6fc610
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-05 20:41:58 +0200

    Section 11. 146. Removing Try_Catch docs.

--- routes/genres.js
+++ routes/genres.js
@@ -1,16 +1,13 @@
+const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
 const express = require('express');
 const router = express.Router();
-  
-router.get('/', async (req, res, next) => {
-	try {
-		const genres = await Genre.find().sort('name');
-		res.send(genres);
-	} catch (ex) {
-		next(ex);
-	}
+ 
+router.get('/', asyncMiddleware(async (req, res) => {
+	const genres = await Genre.find().sort('name');
+	res.send(genres);
 });
   
 router.post('/', auth, async (req, res) => {
