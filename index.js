94ada9f1b6536fcdee067a06a495da71501ee818
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-05 23:22:05 +0200

    Section 11. 151. Unhandled Promise Rejections.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
+// const { createLogger, transports, format } = require('winston');
 require('winston-mongodb');
 const error = require('./middleware/error');
 const config = require('config');
@@ -15,19 +16,38 @@ const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
+/*
 process.on('uncaughtException', (ex) => {
-	console.log('WE GOT AN ENCAUGHT EXCEPTION.');
+	// console.log('WE GOT AN ENCAUGHT EXCEPTION.');
 	winston.error(ex.message, ex);
+	process.exit(1);
+});
+*/
+
+process.on('unhandledRejection', (ex) => {
+	throw ex;
+	/*
+	// console.log('WE GOT AN UNHANDLED REGECTION.');
+	winston.error(ex.message, ex);
+	process.exit(1);
+	*/
 });
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.Console({
-	format: winston.format.simple()
-}));
+winston.add(new winston.transports.Console({ format: winston.format.simple() }));
 winston.add(new winston.transports.MongoDB({ 
 	db: 'mongodb://localhost/vidly',
 	level: 'info'
 }));
+winston.add(new winston.transports.File({
+	filename: 'exceptions.log',
+	handleExceptions: true
+}));
+winston.exitOnError = false;
+
+// Unhandled promise rejection
+// const p = Promise.reject(new Error('Something failed miserably!'));
+// p.then(() => console.log('Done'));
 
 throw new Error('Something failed during startup.');
 
