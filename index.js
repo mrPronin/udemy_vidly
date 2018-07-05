1a3e79bcb71dff5a61cb48fae0516d606c92d6da
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-05 21:27:00 +0200

    Section 11. 148. Logging Errors.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const winston = require('winston');
 const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
@@ -12,6 +13,11 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+winston.add(new winston.transports.Console({
+	format: winston.format.simple()
+}));
 
 if (!config.get('jwtPrivateKey')) {
 	console.log('FATAL ERROR: vidly_jwtPrivateKey is not defined.');
--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,21 @@
+const winston = require('winston');
+
+/* eslint-disable no-unused-vars */
 
 module.exports = function(err, req, res, next) {
 	// Log the exception
+	// logging levels
+	// error
+	// warn
+	// info
+	// verbose
+	// debug
+	// silly
+
+	// winston.log('error', err.message);
+	winston.error(err.message, err);
+	
 	res.status(500).send('Something failed.');
-	if (!next) {
-		console.log('next');
-	}
 };
+
+/* eslint-enable no-unused-vars */
