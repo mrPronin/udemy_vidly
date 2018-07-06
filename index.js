be217ab4316cae2f859cb622f89f00d2d213b7e9
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-06 13:51:16 +0200

    Section 11. 154. Extracting the DB Logic.

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ require('express-async-errors');
 const winston = require('winston');
 // const { createLogger, transports, format } = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
+require('./startup/db')();
 
 /*
 process.on('uncaughtException', (ex) => {
@@ -49,26 +43,12 @@ winston.exitOnError = false;
 // const p = Promise.reject(new Error('Something failed miserably!'));
 // p.then(() => console.log('Done'));
 
-throw new Error('Something failed during startup.');
+// throw new Error('Something failed during startup.');
 
 if (!config.get('jwtPrivateKey')) {
 	console.log('FATAL ERROR: vidly_jwtPrivateKey is not defined.');
 	process.exit(1);
 }
-
-mongoose.connect('mongodb://localhost/vidly')
-	.then(() => console.log('Connected to MongoDB...'))
-	.catch(err => console.error(`Could not connect to MongoDB with error: ${err.message}`));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
