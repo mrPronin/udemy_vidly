754b7f1342ec0a2057408633a84b62210123d4dd
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-22 22:57:31 +0200

    Section 13. 190. Testing Invalid Inputs.

--- models/genre.js
+++ models/genre.js
@@ -2,13 +2,14 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const minlength = 3;
+const maxlength = 50;
 
 const genreSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
 		minlength: minlength,
-		maxlength: 50
+		maxlength: maxlength
 	}
 });
 
@@ -16,7 +17,7 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
 	const schema = {
-		name: Joi.string().min(minlength).required()
+		name: Joi.string().min(minlength).max(maxlength).required()
 	};
 	return Joi.validate(genre, schema);
 }
--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const {User} = require('../../models/user');
 
 let server;
 
@@ -51,7 +52,30 @@ describe('/api/genres', () => {
 
 			expect(res.status).toBe(401);
 		});
-	});
-	
+
+		it('should return 400 if genre is less than 3 characters', async () => {
+			const token = new User().generateAuthToken();
+
+			const res = await request(server)
+				.post('/api/genres')
+				.set('x-auth-token', token)
+				.send({ name: '12' });
+
+			expect(res.status).toBe(400);
+		});
+
+		it('should return 400 if genre is more than 50 characters', async () => {
+			const token = new User().generateAuthToken();
+
+			const name = new Array(52).join('a');
+
+			const res = await request(server)
+				.post('/api/genres')
+				.set('x-auth-token', token)
+				.send({ name: name });
+
+			expect(res.status).toBe(400);
+		});
+	});	
 });
 /* eslint-enable no-undef */
