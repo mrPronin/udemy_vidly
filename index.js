beb5366845fbb13a80f01baf560a3787a0111b76
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-24 18:17:00 +0200

    Section 15. 216. Preparing the App for Production.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require('./startup/db')();
 
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
