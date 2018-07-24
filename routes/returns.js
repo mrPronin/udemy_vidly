01c0c787329ceb6a13fd77ee6ca23980840190fc
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-07-24 17:47:16 +0200

    Section 14. 212. Refactoring the Validation Logic.

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {Rental} = require('../models/rental');
-// const {Movie} = require('../../models/movie');
 const {Movie} = require('../models/movie');
 const auth = require('../middleware/auth');
 const moment = require('moment');
- 
-router.post('/', auth, async (req, res) => {
-	if (!req.body.customerId) return res.status(400).send('customerId not provided');
-	if (!req.body.movieId) return res.status(400).send('movieId not provided');
+const Joi = require('joi');
+const validate = require('../middleware/validate');
+
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 	
 	const rental = await Rental.findOne({
 		'customer._id': req.body.customerId,
@@ -30,5 +29,13 @@ router.post('/', auth, async (req, res) => {
 
 	return res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+	const schema = {
+		customerId: Joi.objectId().required(),
+		movieId: Joi.objectId().required()
+	};
+	return Joi.validate(req, schema);
+}
 
 module.exports = router;
